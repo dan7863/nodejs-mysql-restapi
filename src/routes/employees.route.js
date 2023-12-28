599ddd2136c59949a306db7a54a11656a8e1ecb6
dan7863 <danrico786@gmail.com>
2023-12-28 13:26:15 -0500

    First Commit

---
+++ src/routes/employees.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  createEmployee,
+  deleteEmployee,
+  getEmployee,
+  getEmployees,
+  partialUpdateEmployee,
+  updateEmployee,
+} from "../controllers/employees.controller.js";
+
+const router = Router();
+
+router.get("/employees", getEmployees);
+router.get("/employees/:id", getEmployee);
+router.post("/employees", createEmployee);
+router.patch("/employees/:id", partialUpdateEmployee);
+router.put("/employees/:id", updateEmployee);
+router.delete("/employees/:id", deleteEmployee);
+
+export default router;
